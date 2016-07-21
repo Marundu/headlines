cf92de7a6cf1dcd69fb0c3ce9c8da11e6e2d32c6
Marundu <marundu@gmail.com>
2016-07-21 10:41:09 +0300

    with Jinja templates

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,22 +7,22 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 			 'cnn': 'http://rss.cnn.com/rss/edition.rss',
 			 'fox': 'http://feeds.foxnews.com/foxnews/latest',
 			 'iol': 'http://www.iol.co.za/cmlink/1.640',
-			 'aj': 'http://www.aljazeera.com/?feed=rss2'}
+			 'aj': 'http://www.aljazeera.com/?feed=rss2',
+			 'mg': 'http://mg.co.za/rss/',
+			 'mash': 'http://feeds.mashable.com/Mashable',
+			 'guard': 'https://www.theguardian.com/world/rss',
+			 'granta': 'http://granta.com/feed/',
+			 'fdn': 'http://feeds.feedburner.com/FeministDailyNews',
+			 'ars_index': 'http://feeds.arstechnica.com/arstechnica/index/',
+			 'ars_oss': 'http://feeds.arstechnica.com/arstechnica/open-source/',
+			 'ars_biz': 'http://feeds.arstechnica.com/arstechnica/business/'}
 
 @app.route('/')
 @app.route('/<publication>')
 
 def get_news(publication='bbc'):
 	feed = feedparser.parse(RSS_FEEDS[publication])
-	first_article = feed['entries'][0]
-	return '''<html
-		<body>
-			<h1>Headlines</h1>
-			<b>{0}</b> <br/>
-			<b>{1}</b> <br/>
-			<p>{2}</p> <br/>
-		</body>
-	</html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
+	return render_template('home.html', article=feed['entries'])
 
 if __name__ == '__main__':
 	app.run(debug=True, port = 7001)
